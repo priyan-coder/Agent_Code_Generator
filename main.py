b2c255083f1c305f73b4533b24056031530362b7
brajamoh <rajamohanpriyan@gmail.com>
2025-04-23 15:59:32 -0400

    llamaparser able to parse pdf and store result in a vector index for a local llm query engine to chat with

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from llama_index.llms.ollama import Ollama
+from llama_parse import LlamaParse
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
+from llama_index.core.embeddings import resolve_embed_model
+from dotenv import load_dotenv
+
+load_dotenv()   # load environment variables from .env file
 
 llm = Ollama(model="mistral", request_timeout=30.0)
-print(llm.complete("What is the capital of France?"))
+parser = LlamaParse(result_type="markdown") # takes the document, pushes it to the cloud, gets parsed, and returns
+
+file_extractor = {".pdf": parser} # whenever a pdf is found, it will be parsed
+documents = SimpleDirectoryReader(input_dir="data", file_extractor=file_extractor).load_data()
+
+embed_model = resolve_embed_model("local:BAAI/bge-m3")
+vector_index = VectorStoreIndex.from_documents(documents=documents, embed_model=embed_model)
+query_engine = vector_index.as_query_engine(llm=llm) # can now use the vector index to question the documents
+
+print(query_engine.query("What are some of the routes in the api?"))
+
