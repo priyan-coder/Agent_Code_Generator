ba3e530e0a41da52f5cac1e528e8ee2dd2901c54
brajamoh <rajamohanpriyan@gmail.com>
2025-04-23 16:59:55 -0400

    added ability to read pdf and generate answers based on that

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from llama_index.llms.ollama import Ollama
 from llama_parse import LlamaParse
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
 from llama_index.core.embeddings import resolve_embed_model
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from prompts import context
 from dotenv import load_dotenv
 
 load_dotenv()   # load environment variables from .env file
@@ -16,5 +19,22 @@ embed_model = resolve_embed_model("local:BAAI/bge-m3")
 vector_index = VectorStoreIndex.from_documents(documents=documents, embed_model=embed_model)
 query_engine = vector_index.as_query_engine(llm=llm) # can now use the vector index to question the documents
 
-print(query_engine.query("What are some of the routes in the api?"))
+# the ability to query the pdf and get the response is given as a tool to the agent
+tools = [
+    QueryEngineTool(query_engine=query_engine, metadata=ToolMetadata(name="api_documentation", 
+        description="this gives documentation about code for an API. Use this for reading docs for the API")),
+    
+]
 
+# a different LLM for code generation
+code_llm = Ollama(model="codellama")
+
+# declare agent
+agent = ReActAgent.from_tools(tools=tools, llm=llm, verbose=True, context=context)
+
+# agent would pick the right tool for the job
+
+while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+    # get the response from the agent
+    result = agent.query(prompt)
+    print(f"Response:\n {result}")
